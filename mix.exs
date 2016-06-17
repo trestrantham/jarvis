ef62e32420ac84ec7a67cf197419b6e3668a4daf
Tres Trantham <tres@trestrantham.com>
2016-06-16 23:55:09 -0700

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -2,34 +2,33 @@ defmodule Jarvis.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :jarvis,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :jarvis,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps
+    ]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :hedwig_slack],
-     mod: {Jarvis, []}]
+    [
+      applications: [
+        :hedwig_mopidy,
+        :hedwig_slack,
+        :logger
+      ],
+      mod: {Jarvis, []}
+    ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:hedwig_slack, "~> 0.1"}
+      {:hedwig_slack, "~> 0.1"},
+      {:hedwig_mopidy, git: "git@github.com:trestrantham/hedwig_mopidy.git"},
+      {:mopidy, git: "git@github.com:trestrantham/mopidy.git"},
+      {:poison, "~> 2.1"}
     ]
   end
 end
